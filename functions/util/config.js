6103441703a146f6885a0edcb842a689f3c2108d
yongtany <yongn1494@gmail.com>
2019-12-03 20:10:09 +0900

    [UPDATE] Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyD0eIyjsMxkQj2yZfMPpxz5DbFjs_EEtRE",
+  authDomain: "bestamigo-20a8b.firebaseapp.com",
+  databaseURL: "https://bestamigo-20a8b.firebaseio.com",
+  projectId: "bestamigo-20a8b",
+  storageBucket: "bestamigo-20a8b.appspot.com",
+  messagingSenderId: "391447006855",
+  appId: "1:391447006855:web:44cda6f0dc846d2cfeb222",
+  measurementId: "G-X07TDDJ6JK"
+};
