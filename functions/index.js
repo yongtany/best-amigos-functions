73b61ad0819de2a30157e2b3fb20142ecc4f5845
yongtany <yongn1494@gmail.com>
2019-12-03 17:48:55 +0900

    [CREATE] User Registration

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyD0eIyjsMxkQj2yZfMPpxz5DbFjs_EEtRE",
+  authDomain: "bestamigo-20a8b.firebaseapp.com",
+  databaseURL: "https://bestamigo-20a8b.firebaseio.com",
+  projectId: "bestamigo-20a8b",
+  storageBucket: "bestamigo-20a8b.appspot.com",
+  messagingSenderId: "391447006855",
+  appId: "1:391447006855:web:44cda6f0dc846d2cfeb222",
+  measurementId: "G-X07TDDJ6JK"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-  admin
+  db
     .firestore()
     .collection('screams')
     .orderBy('createdAt', 'desc')
@@ -19,7 +33,9 @@ app.get('/screams', (req, res) => {
           screamId: doc.id,
           body: doc.data().body,
           userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount
         });
       });
       return res.json(screams);
@@ -34,7 +50,7 @@ app.post('/scream', (req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin
+  db
     .firestore()
     .collection('screams')
     .add(newScream)
@@ -47,6 +63,53 @@ app.post('/scream', (req, res) => {
     });
 });
 
-// https://baseurl.com/api/
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: 'this handle is already taken' });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already is use' });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
 
 exports.api = functions.https.onRequest(app);
