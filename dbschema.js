4b76ca2329a8414ab071cfcc84e53f2d91cb8973
yongtany <yongn1494@gmail.com>
2019-12-04 11:38:57 +0900

    [UPDATE] Add user schema

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: 'dklfnalkdnaflkan',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-12-03T07:59:14.695Z',
+      imageUrl: 'image/dlfnalkdnfal/ondlkfnalkf',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'Seoul, Republic of Korea'
+    }
+  ],
   screams: [
     {
       userHandle: 'user',
