07883208fdaaab277bb0ff512579ccafe071e3a0
yongtany <yongn1494@gmail.com>
2019-12-04 12:13:50 +0900

    [CREATE] Add and Get User Profile Details

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,28 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./controllers/screams');
-const { signup, login, uploadImage } = require('./controllers/users');
+const { 
+  getAllScreams, 
+  postOneScream 
+} = require('./controllers/screams');
+const { 
+  signup, 
+  login, 
+  uploadImage, 
+  addUserDetails, 
+  getAuthenticatedUser 
+} = require('./controllers/users');
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 
+
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage)
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
