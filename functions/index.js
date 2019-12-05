df100896be2e017d6fcc96f4442260dbd4147419
yongtany <yongn1494@gmail.com>
2019-12-05 19:26:06 +0900

    [CREATE] Delete Scream data trigger

--- functions/index.js
+++ functions/index.js
@@ -120,4 +120,41 @@ exports.createNotificationOnComment = functions
     } else return true;
   });
 
+exports.onScreamDelete = functions
+.region('asia-northeast1')
+.firestore.document('/screams/{screamId}')
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = db.batch();
+    return db
+      .collection('comments')
+      .where('screamId', '==', screamId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db
+          .collection('likes')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db
+          .collection('notifications')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
+  });
+
     
