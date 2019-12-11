781a647b03e29d3157f7caa1b692e89f849c1cd6
yongtany <yongn1494@gmail.com>
2019-12-11 16:18:29 +0900

    [CREATE] Cors middlw ware

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,11 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
+const cors = require('cors');
+app.use(cors());
+
 const { db } = require('./util/admin');
+
 
 const { 
   getAllScreams, 
